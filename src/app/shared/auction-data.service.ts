47a2b2c21cd7766b3687da3c4d7c18630e19013d
Pascal Sager <sagerpa1@students.zhaw.ch>
2020-03-26 13:35:55 +0100

    task#18 load one auction from api

--- src/app/shared/auction-data.service.ts
+++ src/app/shared/auction-data.service.ts
@@ -1,27 +1,53 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 import {Auction} from './auction';
 import {AUCTION_DATA} from './auction-data';
-import {Observable} from 'rxjs';
-import { of } from 'rxjs';
-import {HttpClient} from '@angular/common/http';
+import {Observable, of} from 'rxjs';
+import {HttpClient} from "@angular/common/http";
+
+
 
 @Injectable()
 export class AuctionDataService {
 
   private auctions: Auction[] = AUCTION_DATA;
+  private URL='http://localhost:4730/auctions';
 
-  constructor(private httpClient: HttpClient) {
-  }
+  constructor(private httpClient: HttpClient) {}
 
   public getAuctions() {
     return this.auctions;
   }
 
-  public getObservableAuctions(): Observable<Auction[]> {
+  getObservableAuctions(): Observable<Auction[]> {
     return of(this.auctions);
   }
 
   public getHttpAuctions(): Observable<Array<Auction>> {
-    return this.httpClient.get<Array<Auction>>('http://localhost:4730/auctions');
+    return this.httpClient.get<Array<Auction>>(this.URL);
   }
+
+  public getHttpAuction(id: number): Observable<Auction> {
+    return this.httpClient.get<Auction>(this.URL + '/' + id);
+  }
+
+  public create(auction: Auction): Observable<Auction> {
+    return this.httpClient.post<Auction>(this.URL, auction);
+  }
+
+  public delete(auction: Auction): Observable<Auction> {
+    return this.httpClient.delete<Auction>(`${this.URL}/${auction.id}`);
+  }
+
+  public get(id: number): Observable<Auction> {
+    return this.httpClient.get<Auction>(this.URL + '/' + id);
+  }
+
+  public list(): Observable<Array<Auction>> {
+    return this.httpClient.get<Array<Auction>>(this.URL);
+  }
+
+  public update(auction: Auction): Observable<Auction> {
+    return this.httpClient.put<Auction>(`${this.URL}/${auction.id}`, auction);
+  }
+
 }
