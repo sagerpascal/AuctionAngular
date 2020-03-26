6eb9ff0467ec4221eddf086d0b1f3fa0573405d5
Pascal Sager <sagerpa1@students.zhaw.ch>
2020-03-26 12:56:02 +0100

    task#13 add routing file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
+
 import {AppComponent} from './app.component';
 import {AuctionListComponent} from './auction-list/auction-list.component';
 import {MouseEventDisplayComponent} from './mouse-event-display/mouse-event-display.component';
---
+++ src/app/app.routing.ts
@@ -0,0 +1,3 @@
+import {Routes, RouterModule} from '@angular/router';
+export const routes: Routes = [];
+export const routing = RouterModule.forRoot(routes);
