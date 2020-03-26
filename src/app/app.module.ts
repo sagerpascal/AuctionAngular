8f78d5087aa045451645939213c1c0f826af70ec
Pascal Sager <sagerpa1@students.zhaw.ch>
2020-03-26 11:43:44 +0100

    task#07 auction data service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AuctionListComponent } from './auction-list/auction-list.component';
 import { MouseEventDisplayComponent } from './mouse-event-display/mouse-event-display.component';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {AuctionDataService} from './shared/auction-data.service';
 
 @NgModule({
   declarations: [
@@ -12,9 +15,11 @@ import { MouseEventDisplayComponent } from './mouse-event-display/mouse-event-di
     MouseEventDisplayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AuctionDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
