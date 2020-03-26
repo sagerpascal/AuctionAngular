74d1d168bad6f8a033574f126b99f110f4e9eab9
Pascal Sager <sagerpa1@students.zhaw.ch>
2020-03-26 12:41:49 +0100

    task#11 component life cycle

--- src/app/shared/auction-data.service.ts
+++ src/app/shared/auction-data.service.ts
@@ -1,17 +1,20 @@
+import { Injectable } from '@angular/core';
 import {Auction} from './auction';
 import {AUCTION_DATA} from './auction-data';
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {Observable, of} from 'rxjs';
 
 @Injectable()
 export class AuctionDataService {
 
   private auctions: Auction[] = AUCTION_DATA;
 
-  constructor(private http: HttpClient) {
-  }
+  constructor() { }
 
   public getAuctions() {
     return this.auctions;
+  }
+
+  getObservableAuctions(): Observable<Auction[]> {
+    return of(this.auctions);
   }
 }
