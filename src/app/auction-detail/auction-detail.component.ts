4871e1b3b24b207e32268e533d41d7ec727c44f1
Pascal Sager <sagerpa1@students.zhaw.ch>
2020-03-26 13:31:52 +0100

    task#17 read route params

--- src/app/auction-detail/auction-detail.component.ts
+++ src/app/auction-detail/auction-detail.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {AuctionDataService} from '../shared/auction-data.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-auction-detail',
   templateUrl: './auction-detail.component.html',
-  styleUrls: ['./auction-detail.component.css']
+  styleUrls: ['./auction-detail.component.scss']
 })
 export class AuctionDetailComponent implements OnInit {
 
-  constructor() { }
+  private routeSubscription: Subscription;
 
-  ngOnInit(): void {
+  constructor(private auctionDataService: AuctionDataService,
+              private route: ActivatedRoute,
+              private router: Router) {
+
   }
 
+  ngOnInit() {
+    this.routeSubscription = this.route.params.subscribe(params => {
+      console.log(params['id']);
+
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.routeSubscription != null) {
+      this.routeSubscription.unsubscribe();
+    }
+  }
 }
