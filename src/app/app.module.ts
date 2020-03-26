143f196fd00fa9cdd5734d00640507d6000c9cca
Pascal Sager <sagerpa1@students.zhaw.ch>
2020-03-26 11:17:52 +0100

    task#02 mouse cursor position

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AuctionListComponent } from './auction-list/auction-list.component';
+import { MouseEventDisplayComponent } from './mouse-event-display/mouse-event-display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuctionListComponent
+    AuctionListComponent,
+    MouseEventDisplayComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/mouse-event-display/mouse-event-display.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-mouse-event-display',
+  templateUrl: './mouse-event-display.component.html',
+  styleUrls: ['./mouse-event-display.component.css']
+})
+export class MouseEventDisplayComponent implements OnInit {
+
+  public event: MouseEvent;
+  public clientX = 0;
+  public clientY = 0;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  public onEvent(event: MouseEvent): void {
+    this.event = event;
+  }
+
+  public coordinates(event: MouseEvent): void {
+    this.clientX = event.clientX;
+    this.clientY = event.clientY;
+  }
+
+}
